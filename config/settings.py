9f842a6184062e31f9942cb31662016ab8e3f837
2005964 <hayeon_1027@naver.com>
2021-11-02 18:43:53 +0900

    add

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'wy+27o!-k3+pdwk)fl*fp*do(9@9kicbqszmtni(173&oec4p^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
